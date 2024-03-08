032fc0b089188ea7314159e127ff9baf1030df84
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-03-08 13:44:07 +0800

    initial setup

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { typeDefs } from "./schema";
+import { resolvers } from "./resolvers";
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+startStandaloneServer(server, {
+  listen: { port: 4000 },
+}).then(({ url }) => {
+  console.log(`Server ready at ${url}`)
+})
